c9d967a56d89328495d0a1dd57a044c3bfd03357
mishinkinakatya <mishinkinakatya@gmail.com>
2020-04-09 01:39:11 +0500

    Таски. Шаблон + моки

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import {createSiteMenuTemplate} from "./components/site-menu.js";
-import {createFilterTemplate} from "./components/filter.js";
-import {createBoardTemplate} from "./components/board.js";
 import {createSortingTemplate} from "./components/sorting.js";
-import {createTaskEditTemplate} from "./components/task-edit.js";
+import {createFilterTemplate} from "./components/filter.js";
 import {createTaskTemplate} from "./components/task.js";
+import {createTaskEditTemplate} from "./components/task-edit.js";
 import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
+import {createBoardTemplate} from "./components/board.js";
+import {generateTasks} from "./mock/task.js";
 import {generateFilters} from "./mock/filter.js";
+
 
 const TASK_COUNT = 3;
 
@@ -13,9 +15,11 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector(`main`);
+const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
 const filters = generateFilters();
+const tasks = generateTasks(TASK_COUNT);
 
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(filters), `beforeend`);
@@ -25,10 +29,10 @@ const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingTemplate(), `afterbegin`);
-render(taskListElement, createTaskEditTemplate(), `beforeend`);
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), `beforeend`);
+for (let i = 1; i < tasks.length; i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
